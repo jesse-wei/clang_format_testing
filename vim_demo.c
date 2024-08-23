228cc84b07efdcd4c0532627f9f57bc32a938bd0
Jesse Wei <jesse@cs.unc.edu>
2024-08-23 06:49:07 -0400

    Update vim_demo.c

--- vim_demo.c
+++ vim_demo.c
@@ -4,12 +4,10 @@ int sum(int a, int b, int c) {
     return a + b + c;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
     int x = 1;
     int y = 2;
     int z = 3;
     int rv = sum(x, y, z);
-    printf("%d + %d + %d = %d\n", x, y, z, rv);
     return 0;
 }
-
